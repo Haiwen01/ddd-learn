bd59da6b2930a7ee88cf62f23e180e1e8b8ab8cf
haiwen <haiwen.lai@gmail.com>
2019-12-13 19:06:10 +0800

    add: init code

---
+++ src/main/java/com/haiwen/dddlearn/DddlearnApplication.java
@@ -0,0 +1,13 @@
+package com.haiwen.dddlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddlearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddlearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haiwen/dddlearn/DddlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haiwen.dddlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddlearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
